bc2bea6c541f8f1f9e2fdbbc5fff7b2906390594
hajni <ksb12213@naver.com>
2020-09-14 11:26:44 +0900

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,61 +1,54 @@
 const jwt = require("jsonwebtoken");
-const chalk = require("chalk");
 const connection = require("../db/mysql_connection");
+const chalk = require("chalk");
 
 let nomal_txt = chalk.cyanBright;
 let highlight_txt = chalk.yellowBright;
 
 const auth = async (req, res, next) => {
-  console.log(chalk.bold("<<  인증 미들웨어 실행됨  >>"));
+  console.log(chalk.bold("<< 인증 미들웨어 실행 됨 >>"));
   let token;
   try {
     token = req.header("Authorization");
     token = token.replace("Bearer ", "");
     console.log(highlight_txt.bold("login token") + nomal_txt(" - " + token));
   } catch (e) {
-    res.status(401).json({ error: e });
+    res.status(401).json();
     return;
   }
 
+  console.log(token);
+
   let user_id;
-  let receiver_id;
   try {
     const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
     user_id = decoded.user_id;
-    receiver_id = decoded.receiver_id;
   } catch (e) {
-    res.status(401).json({ error: e, message: "여기" });
+    res.status(401).json();
     return;
   }
 
   let query =
-    "select u.id, u.email, u.nickname, u.created_at, u.name , t.token, t.receiver_id\
-  from p_token as t \
-  join p_user as u \
-  on t.user_id = u.id \
-  where t.user_id = ? and t.token = ? and t.receiver_id = ?";
-  console.log(query);
+    "select u.id, u.email, u.created_at, t.token \
+  from p_user as u \
+  join p_token as t \
+  on u.id = t.user_id \
+  where t.user_id = ? and t.token = ?;";
 
-  let data = [user_id, token, receiver_id];
+  let data = [user_id, token];
 
   try {
     [rows] = await connection.query(query, data);
     if (rows.length == 0) {
-      res.status(401).json({ error: e });
+      res.status(401).json();
       return;
     } else {
       req.user = rows[0];
-      console.log(
-        highlight_txt.bold("User authorization") +
-          nomal_txt(" user_id : ") +
-          highlight_txt(user_id) +
-          nomal_txt(", email : ") +
-          highlight_txt(rows[0].email)
-      );
       next();
     }
   } catch (e) {
-    res.status(500).json({ error: e });
+    res.status(500).json();
+    return;
   }
 };
 
