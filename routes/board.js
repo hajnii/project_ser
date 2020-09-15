688c31c145b834acf24006a841f954aef5ecb676
hajni <ksb12213@naver.com>
2020-09-15 15:51:19 +0900

    권혁준 죽는다

--- routes/board.js
+++ routes/board.js
@@ -12,7 +12,7 @@ const router = express.Router();
 
 // 각 경로별로 데이터 가져올 수 있도록, router 셋팅
 router.route("/").post(auth, BoardUpload).get(getBoardlist);
-router.route("/view").post(auth, viewBoard);
+router.route("/view").post(viewBoard);
 router.route("/update").post(auth, updateBoard);
 router.route("/delete").delete(auth, deleteBoard);
 router.route("/search").post(searchBoard);
