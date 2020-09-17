60ebb5729b64e36152d0090b4546867902559a67
hajni <ksb12213@naver.com>
2020-09-17 11:02:41 +0900

    second commit

--- routes/board.js
+++ routes/board.js
@@ -7,12 +7,14 @@ const {
   deleteBoard,
   viewBoard,
   searchBoard,
+  noauthviewBoard,
 } = require("../controllers/board");
 const router = express.Router();
 
 // 각 경로별로 데이터 가져올 수 있도록, router 셋팅
 router.route("/").post(auth, BoardUpload).get(getBoardlist);
 router.route("/view").post(auth, viewBoard);
+router.route("/noauthview").post(noauthviewBoard);
 router.route("/update").post(auth, updateBoard);
 router.route("/delete").delete(auth, deleteBoard);
 router.route("/search").post(searchBoard);
