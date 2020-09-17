f6fa2199df3baa406b0869a1b58940cbd48418c4
hajni <ksb12213@naver.com>
2020-09-17 13:58:03 +0900

    조회 수정

--- routes/board.js
+++ routes/board.js
@@ -6,14 +6,14 @@ const {
   updateBoard,
   deleteBoard,
   searchBoard,
-  noauthviewBoard,
+  viewBoard,
 } = require("../controllers/board");
 const router = express.Router();
 
 // 각 경로별로 데이터 가져올 수 있도록, router 셋팅
 router.route("/").post(auth, BoardUpload).get(getBoardlist);
 // router.route("/view").post(auth, viewBoard);
-router.route("/noauthview").post(noauthviewBoard);
+router.route("/view").post(auth, viewBoard);
 router.route("/update").post(auth, updateBoard);
 router.route("/delete").delete(auth, deleteBoard);
 router.route("/search").post(searchBoard);
