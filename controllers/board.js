24d8048b33acb098cf2298ba41b56d6cf1ba02d7
hajni <ksb12213@naver.com>
2020-09-25 16:46:36 +0900

    쓴글 가져왹

--- controllers/board.js
+++ controllers/board.js
@@ -290,13 +290,29 @@ exports.myWrite = async (req, res, next) => {
   let user_id = req.user.id;
   let offset = req.query.offset;
   let limit = req.query.limit;
-  let query = `
-              select 'board' as type,board_id,null as question_id,title,b.category,content,b.created_at,u.nickname,u.email,(select count(*) from p_boardview where board_id = b.board_id) as view_cnt,(select count(*) from p_comment where board_id = b.board_id) as com_cnt,b.starttime,b.endtime
-              from p_board b join p_user u on b.user_id = u.id
+  let board_id = req.body.board_id;
+
+  // 해당 유저의 게시글이 맞는지 체크
+  let query = `select * from p_user where id = ${user_id}`;
+
+  try {
+    [rows] = await connection.query(query);
+    if (rows[0].user_id == user_id) {
+      console.log("@@@@@@@@@@", rows[0].user_id);
+      res.status(401).json({ seccess: false });
+      return;
+    }
+  } catch (e) {
+    res.status(500).json({ message: "위치 확인", error: e });
+    return;
+  }
+
+  query = `
+              select 'board' as type,board_id,null as question_id,title,b.category,content,b.created_at,u.nickname,u.email,b.user_id,(select count(*) from p_boardview where board_id = b.board_id) as view_cnt,(select count(*) from p_comment where board_id = b.board_id) as com_cnt,b.starttime,b.endtime
+              from p_board b join p_user u on b.user_id = u.id where user_id = ${user_id}
               union
-              select 'question' as type, null as board_id,question_id as board_id,title,q.category,content,q.created_at, u.nickname,u.email,(select count(*) from p_boardview where question_id =q. question_id) as view_cnt,(select count(*) from p_comment where question_id = q. question_id) as com_cnt,null as starttime,null asendtime
-              from p_question q join p_user u on q.user_id = u.id
-              order by created_at desc limit ${offset},${limit}
+              select 'question' as type, null as board_id,question_id as board_id,title,q.category,content,q.created_at, u.nickname,u.email,q.user_id,(select count(*) from p_boardview where question_id =q. question_id) as view_cnt,(select count(*) from p_comment where question_id = q. question_id) as com_cnt,null as starttime,null asendtime
+              from p_question q join p_user u on q.user_id = u.id where user_id = ${user_id} order by created_at desc limit ${offset},${limit}
             `;
   console.log(query);
   try {
